aebd8d01b62bd9c3add80929cadb549ebd08a05a
AHINSA-BOOST <nowseedbuy@gmail.com>
2026-03-05 15:09:55 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,27 +13,27 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         // Generate system status message
-        const status = `╭━━〔 *CHAMA-MD* 〕━━┈⊷
+        const status = `╭━━〔 *Queen_Ahinsa-MD* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *⏳Uptime*:  ${runtime(process.uptime())} 
 ┃◈┃• *📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
 ┃◈┃• *⚙️ HostName*: ${os.hostname()}
-┃◈┃• *👨‍💻 Owner*: Chamindu
+┃◈┃• *👨‍💻 Owner*: 𝙳𝙸𝙻𝙴𝙴𝚂𝙷𝙰 𝙶𝙸𝙼𝚂𝙷𝙰𝙽
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ chamindu`;
+> © 𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈 𝙺𝙸𝙽𝙶 𝙳𝙸𝙻𝙴𝙴𝚂𝙷𝙰`;
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
-            image: { url: `https://i.ibb.co/5XNzHkVp/3908.jpg` },  // Image URL
+            image: { url: `https://files.catbox.moe/xnl46e.jpg` },  // Image URL
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363419192353625@newsletter',
-                    newsletterName: '☈☟𝗖𝗛𝗔𝗠𝗔 𝗠𝗗',
+                    newsletterJid: '120363425599106790@newsletter',
+                    newsletterName: '𝐐𝐮𝐞𝐞𝐧_𝐀𝐡𝐢𝐧𝐬𝐚-𝐌𝐃',
                     serverMessageId: 143
                 }
             }
